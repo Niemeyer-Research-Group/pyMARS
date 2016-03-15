f9f674d6e1672d805e1c2db6be5c093c8df29dcc
parkerclayton <parkerclayton@comcast.net>
2016-03-15 09:57:31 -0700

    modified variable names

--- create_trimmed_model.py
+++ create_trimmed_model.py
@@ -19,35 +19,35 @@ def create_trimmed_model(data_file, exclusion_list):
         Short list of solution reaction objects
     New :
         Simplified Cantera Solution
-
     """
 
-    Solution = ct.Solution(data_file)
-    Species = Solution.species_names
+    initial_solution = ct.Solution(data_file)
+    initial_species  = initial_solution.species_names
 
     for n in exclusion_list:
-            if n in Species:
-                Species.remove(n)
-    SpeciesObjects	=	[Solution.species(name) for name in Species]
+            if n in initial_species:
+                initial_species.remove(n)
 
-    ReactionList 	= 	Solution.reaction_equations()
-    ReactionObjects	=	Solution.reaction
+    Species_Objects =   [initial_solution.species(name) for name in initial_species]
+
+    ReactionList    = 	initial_solution.reaction_equations()
+    ReactionObjects =	initial_solution.reaction
     list	=	[]
     for i, Rxn in enumerate(ReactionList):
         Reactants 	= 	ReactionObjects(i).reactants
         Products	= 	ReactionObjects(i).products
         for k in exclusion_list:
             if k not in Reactants and k not in Products:
-                list.append(Solution.reaction(i))
+                list.append(initial_solution.reaction(i))
 
     ReactionObjects = list
-    New= ct.Solution(species=SpeciesObjects, reactions=ReactionObjects,
+    new_solution= ct.Solution(species=Species_Objects, reactions=ReactionObjects,
                     thermo='IdealGas', kinetics='GasKinetics')
-    print(Solution())
-    print(New())
+    print(initial_solution.n_reactions)
+    print(new_solution.n_reactions)
 
 
 #calling the function
 #list to exclude
 SPexc=['O2'];
-datareadin("gri30.cti", SPexc)
+create_trimmed_model("gri30.cti", SPexc)
