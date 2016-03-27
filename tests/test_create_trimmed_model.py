cbea837e630523a7a51063fc7dd4f7f535603f2a
parkerclayton <parkerclayton@comcast.net>
2016-03-26 21:28:23 -0700

    renamed test file

--- tests/test_create_trimmed_model.py
+++ tests/test_create_trimmed_model.py
@@ -30,11 +30,15 @@ new_solution = result[1]
 n_initial_species= len(initial_solution.species())
 n_new_species= len(new_solution.species())
 
-def test():
+def test_n_species():
     assert n_initial_species > n_new_species
 
+n_initial_reactions = len(initial_solution.reactions())
+n_new_reactions = len(new_solution.reactions())
 
-print(n_new_species)
+def test_n_reactions():
+    assert n_initial_reactions > n_new_reactions
+
 
 #output for user
 #print('start %s initial species, %s initial reactions') %(initial_solution.n_species, initial_solution.n_reactions)
