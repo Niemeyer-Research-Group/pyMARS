f197fd60c12ad6161db9b17826f489882d8c0313
parkerclayton <parkerclayton@comcast.net>
2016-04-06 13:49:48 -0700

    modified convert file to output converted file to output_data_files

--- convert_chemkin_file.py
+++ convert_chemkin_file.py
@@ -4,7 +4,12 @@
 import os
 
 def convert(data_file):
+    output_dir= 'Output_Data_Files'
+    current_dir=os.path.dirname(os.path.abspath("~"))
+    save_path=os.path.join(current_dir, output_dir)
+
     converted_file_name=os.path.splitext(data_file)[0]+'_converted'
-    input_line= "ck2cti --input=%s --output=%s" %(data_file, converted_file_name)
+    converted_file_path=os.path.join(save_path, converted_file_name)
+    input_line= "ck2cti --input=%s --output=%s" %(data_file, converted_file_path)
     os.system(input_line)
     return converted_file_name + '.cti'
