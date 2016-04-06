3429543454427f93d6c410f95a06f18406e3101f
parkerclayton <parkerclayton@comcast.net>
2016-04-06 15:09:26 -0700

    modified function to take input from Input Data Files folder and output converted file to Output Data Files

--- convert_chemkin_file.py
+++ convert_chemkin_file.py
@@ -5,11 +5,14 @@ import os
 
 def convert(data_file):
     output_dir= 'Output_Data_Files'
+    input_dir='Input_Data_Files'
     current_dir=os.path.dirname(os.path.abspath("~"))
     save_path=os.path.join(current_dir, output_dir)
 
     converted_file_name=os.path.splitext(data_file)[0]+'_converted'
     converted_file_path=os.path.join(save_path, converted_file_name)
-    input_line= "ck2cti --input=%s --output=%s" %(data_file, converted_file_path)
+
+    input_path=os.path.join(input_dir, data_file)
+    input_line= "ck2cti --input=%s --output=%s" %(input_path, converted_file_path)
     os.system(input_line)
     return converted_file_name + '.cti'
