411e968dd85daa52450a054d49cd96a05a485c2d
parkerclayton <parkerclayton@comcast.net>
2016-04-07 15:48:32 -0700

    update

--- convert_chemkin_file.py
+++ convert_chemkin_file.py
@@ -1,19 +1,20 @@
 #converts Chemkin file to Cantera format
 
-
 import os
 
 def convert(data_file):
     input_dir='Input_Data_Files'
     output_dir= 'Output_Data_Files'
 
-
+    #make file save path
     current_dir=os.path.dirname(os.path.abspath("~"))
     save_path=os.path.join(current_dir, output_dir)
 
+    #make file save name
     converted_file_name=os.path.splitext(data_file)[0]+'_converted'
     converted_file_path=os.path.join(save_path, converted_file_name)
 
+    #convert and save file
     input_path=os.path.join(input_dir, data_file)
     input_line= "ck2cti --input=%s --output=%s" %(input_path, converted_file_path)
     os.system(input_line)
