ba5ac9544afff8f6c0dda4cdc856fcdb9044b79c
parkerclayton <parkerclayton@comcast.net>
2016-04-13 16:31:06 -0700

    write to cti currently works for writing properly formatted reactions. next step is towork on the species-writing part of the function

--- write_to_cti.py
+++ write_to_cti.py
@@ -3,7 +3,6 @@
 currently only works for Elementary, Falloff and ThreeBody Reactions"""
 
 from identify_file_extension import readin
-import re
 
 A=readin('gri30.cti', [])
 initial=A[0]
@@ -16,7 +15,11 @@ os.system('rm -r testfile.cti')
 f=open('testfile.cti', 'w+')
 
 
-'Intro Block for Reactions'
+
+"""-----------------------------------------------------------------------------
+Write reactions to file
+-----------------------------------------------------------------------------"""
+
 f.write('#'+ "-"*80 + '\n')
 f.write('#  Reaction Data\n')
 f.write('#'+ "-"*80 + '\n\n')
@@ -25,9 +28,9 @@ for n, i in enumerate(final.reaction_equations()):
     equation_string=final.reaction_equation(n)
     equation_object=final.reaction(n)
     equation_type=type(equation_object).__name__
+    m=str(n+1)
     if equation_type == 'ThreeBodyReaction':
         Arr=["{:.2E}".format(equation_object.rate.pre_exponential_factor), equation_object.rate.temperature_exponent, equation_object.rate.activation_energy]
-        m=str(n+1)
         Efficiencies=str(equation_object.efficiencies).replace("{", "\"").replace("\'", "").replace(": ", ":").replace(",", " ").replace("}", "\"")
         f.write('#  ' + 'Reaction' + ' ' + m + '\n')
         f.write('three_body_reaction(  ' + '\"'+ equation_string + '\",   ' + str(Arr))
@@ -35,15 +38,23 @@ for n, i in enumerate(final.reaction_equations()):
 
     if equation_type == 'ElementaryReaction':
         Arr=["{:.2E}".format(equation_object.rate.pre_exponential_factor), equation_object.rate.temperature_exponent, equation_object.rate.activation_energy]
-        m=str(n+1)
         f.write('#  ' + 'Reaction' + ' ' + m + '\n')
         f.write('reaction( ' + '\"'+ equation_string + '\",   ' + str(Arr)  + ')'+ '\n\n')
 
     if equation_type == 'FalloffReaction':
-        m=str(n+1)
-        Efficiencies=str(equation_object.efficiencies).replace("{", "\"").replace("\'", "").replace(": ", ":").replace(",", " ")
+        Efficiencies=str(equation_object.efficiencies).replace("{", "\"").replace("\'", "").replace(": ", ":").replace(",", " ").replace("}", "\"")
+        kf=["{:.2E}".format(equation_object.high_rate.pre_exponential_factor), equation_object.high_rate.temperature_exponent, equation_object.high_rate.activation_energy]
+        kf0=["{:.2E}".format(equation_object.low_rate.pre_exponential_factor), equation_object.low_rate.temperature_exponent, equation_object.low_rate.activation_energy]
+        j=equation_object.falloff.parameters
         f.write('#  ' + 'Reaction' + ' ' + m + '\n')
-        f.write('falloff_reaction(  ' + '\"'+ equation_string + '\",   '   + ')')
-        f.write('\n          ' + 'efficiencies = '+ Efficiencies + ')' + '\n\n')
+        f.write('falloff_reaction(  ' + '\"'+ equation_string + '\",   ' +'\n'  )
+        f.write('           kf = ' + str(kf).replace("\'", "")+ '\n')
+        f.write('           kf0 = ' + str(kf0).replace("\'", ""))
+        try:
+            falloff_str=str('\n' + '           falloff = Troe(A = ' + str(j[0]) + ', T3 = ' + str(j[1]) + ', T1 = ' + str(j[2]) + ', T2 = ' + str(j[3]))
+            f.write(falloff_str)
+        except (IndexError):
+            pass
+        f.write('\n          ' + ' efficiencies = '+ Efficiencies + ')' + '\n\n')
 
 os.system('atom testfile.cti')
