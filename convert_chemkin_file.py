f67f95f08ae9ec3424f5b01d867f4558eadbfa66
parkerclayton <parkerclayton@comcast.net>
2016-04-27 00:07:53 -0700

    added ethanol mechanism files from LNLL for testing

--- convert_chemkin_file.py
+++ convert_chemkin_file.py
@@ -2,7 +2,7 @@
 
 import os
 
-def convert(data_file):
+def convert(mech_file, thermo_file, transport_file):
     input_dir='Input_Data_Files'
     output_dir= 'Output_Data_Files'
 
@@ -11,12 +11,14 @@ def convert(data_file):
     save_path=os.path.join(current_dir, output_dir)
 
     #make file save name
-    converted_file_name=os.path.splitext(data_file)[0]+'_converted'
+    converted_file_name=os.path.splitext(mech_file)[0]+'_converted'
     converted_file_path=os.path.join(save_path, converted_file_name)
 
     #convert and save file
-    input_path=os.path.join(input_dir, data_file)
-    input_line= "ck2cti --input=%s --output=%s" %(input_path, converted_file_path)
+    mech_file=os.path.join(input_dir, mech_file)
+    thermo_file=os.path.join(input_dir, thermo_file)
+    transport_file=os.path.join(input_dir, transport_file)
+    input_line= "ck2cti -i=%s -t=%s -tr=%s --output=%s" %(mech_file, thermo_file, transport_file, converted_file_path)
     os.system(input_line)
 
     local_path=os.path.join(output_dir, converted_file_name)
--- tests/test_write_to_cti.py
+++ tests/test_write_to_cti.py
@@ -3,7 +3,8 @@ import cantera as ct
 import os
 
 parent_dir=os.path.dirname(os.getcwd())
-from parent_dir import write_to_cti
+print(parent_dir)
+
 
 def test_solution_object():
     initial_solution_object=ct.Solution('gri30.cti')
