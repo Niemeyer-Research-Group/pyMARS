378524991280eb4d053dbac0bacd0a1f48619768
parkerclayton <parkerclayton@comcast.net>
2016-05-03 00:06:26 -0700

    tests conversion of LNLL h2 mechanism to cantera format

---
+++ tests/test_convert_chemkin_h2.py
@@ -0,0 +1,40 @@
+"""Tests the convert_chemkin_file using LNLL h2 mechanism files
+
+Chemkin mechanism, thermo and transport data files are imported to
+the input folder and convert_chemkin_file is called.
+
+Complete Cantera mechanism file is left in output folder"""
+
+"""Note: this test can be run from py.test, or independently"""
+
+"""
+file:
+    convert_chemkin_file.py
+
+test parameters:
+    Ck2cti works, and produces a mechanism file that can be used
+    by Cantera to create a Soltuion
+"""
+
+
+import cantera as ct
+from convert_chemkin_file import convert
+import os
+
+def test_is_solution():
+    #setup
+    os.chdir('Data_Files')
+    h2_package=['h2_v1b_mech.dat', 'h2_v1a_therm.dat', 'h2_v1a_tran.dat'  ]
+    for i, files in enumerate(h2_package):
+        os.system('cp ' + h2_package[i] +' ../Input_Data_Files')
+    os.chdir('../')
+
+    #test cti file
+    file_name=convert('h2_v1b_mech.dat', 'h2_v1a_therm.dat', 'h2_v1a_tran.dat')
+    solution=ct.Solution(file_name)
+    assert solution.__qualname__ == 'Solution'
+
+    #teardown
+    os.chdir('Input_Data_Files')
+    for i, files in enumerate(h2_package):
+        os.system('rm ' + h2_package[i])
