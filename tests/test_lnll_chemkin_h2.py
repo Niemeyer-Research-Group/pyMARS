16be30c473c651363681a3e496a858c94587e0ab
parkerclayton <parkerclayton@comcast.net>
2016-05-04 00:52:05 -0700

    fixed so it should copy and clean up files to input directory as needed

--- tests/test_lnll_chemkin_h2.py
+++ tests/test_lnll_chemkin_h2.py
@@ -15,8 +15,22 @@ test parameters:
 import cantera as ct
 from convert_chemkin_file import convert
 import os
+#import __main__
 
 def test_convert_is_solution():
+    #setup
+    os.chdir('Data_Files')
+    h2_package=['h2_v1b_mech.dat', 'h2_v1a_therm.dat', 'h2_v1a_tran.dat'  ]
+    for i, files in enumerate(h2_package):
+        os.system('cp ' + h2_package[i] +' ../Input_Data_Files')
+    os.chdir('../')
+
+    #test cti file
     file_name=convert('h2_v1b_mech.dat', 'h2_v1a_therm.dat', 'h2_v1a_tran.dat')
     solution=ct.Solution(file_name)
     assert solution.__qualname__ == 'Solution'
+
+    #teardown
+    os.chdir('Input_Data_Files')
+    for i, files in enumerate(h2_package):
+            os.system('rm ' + h2_package[i])
