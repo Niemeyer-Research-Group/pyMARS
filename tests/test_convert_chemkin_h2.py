05d60128d890195da3fa6abeab07af3998f0e1f9
parkerclayton <parkerclayton@comcast.net>
2016-05-04 00:14:55 -0700

    simplified code. can only be run using pytest, but does not require os operations

--- tests/test_convert_chemkin_h2.py
+++ tests/test_convert_chemkin_h2.py
@@ -1,11 +1,6 @@
-"""Tests the convert_chemkin_file using LNLL h2 mechanism files
-
-Chemkin mechanism, thermo and transport data files are imported to
-the input folder and convert_chemkin_file is called.
-
+"""Tests LNLL h2 mechanism files
 Complete Cantera mechanism file is left in output folder"""
 
-"""Note: this test can be run from py.test, or independently"""
 
 """
 file:
@@ -21,20 +16,7 @@ import cantera as ct
 from convert_chemkin_file import convert
 import os
 
-def test_is_solution():
-    #setup
-    os.chdir('Data_Files')
-    h2_package=['h2_v1b_mech.dat', 'h2_v1a_therm.dat', 'h2_v1a_tran.dat'  ]
-    for i, files in enumerate(h2_package):
-        os.system('cp ' + h2_package[i] +' ../Input_Data_Files')
-    os.chdir('../')
-
-    #test cti file
+def test_convert_is_solution():
     file_name=convert('h2_v1b_mech.dat', 'h2_v1a_therm.dat', 'h2_v1a_tran.dat')
     solution=ct.Solution(file_name)
     assert solution.__qualname__ == 'Solution'
-
-    #teardown
-    os.chdir('Input_Data_Files')
-    for i, files in enumerate(h2_package):
-        os.system('rm ' + h2_package[i])
