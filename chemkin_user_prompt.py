041e8d5ae9a8cf5f4ba41db4087e11f1d44326b0
parkerclayton <parkerclayton@comcast.net>
2016-05-05 02:15:31 -0700

    added user prompt for chemkin thermo and transport files

--- chemkin_user_prompt.py
+++ chemkin_user_prompt.py
@@ -1,6 +1,13 @@
 #prompts user for chemkin thermo and transport data files
 
 def ask():
-    thermo_name=str(raw_input('Enter thermo data file with extension: '))
-    transport_name=str(raw_input('Enter transport data file with extension: '))
+    q1=str(raw_input('Separate thermo file? [y/n]: '))
+    q2=str(raw_input('Separate transport file? [y/n]: '))
+    if q1 == 'y':
+        thermo_name=str(raw_input('Enter thermo data file with extension: '))
+    else:
+        thermo_name='none'
+    if q2 == 'y':
+        transport_name=str(raw_input('Enter transport data file with extension: '))
+    else: transport_name='none'
     return (thermo_name, transport_name)
