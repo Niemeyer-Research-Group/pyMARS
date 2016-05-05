2a9b9d8127902d7c5557bfc2422d97ada551e972
parkerclayton <parkerclayton@comcast.net>
2016-05-05 02:16:48 -0700

    not working right now. needs more testing to determine the issue

--- __main__.py
+++ __main__.py
@@ -2,6 +2,10 @@
 
 import cantera as ct
 import os
+import sys
+
+
+solution_objects=[]
 
 def readin(data_file, exclusion_list):
     """Function to import data file and identify format.
@@ -10,16 +14,15 @@ def readin(data_file, exclusion_list):
     ----------
     data_file:
         Local Chemkin or Cantera data file containing mechanism information
-
+    exclusion_list:
+        list of species to trim
     Returns
     -------
         Converted mechanism file
         Trimmed Solution Object
         Trimmed Mechanism file
     """
-
-    global Result
-
+    global solution_objects
     #import working functions
     from create_trimmed_model import create_trimmed_model
     from convert_chemkin_file import convert
@@ -31,20 +34,23 @@ def readin(data_file, exclusion_list):
         #trims file
         solution_objects=create_trimmed_model(data_file, exclusion_list)
 
-    elif data_file.endswith(".inp") or dat_file.endswith('.dat') \
+    if data_file.endswith(".inp") or data_file.endswith('.dat') \
                 or data_file.endswith('.txt'):
         print("This is a Chemkin file")
         ask()
         #convert file to cti
-        converted_file_name = convert(data_file, thermo_file, transport_file)
+        converted_file_name = convert(data_file)
 
         #trims newly converted file
         solution_objects=create_trimmed_model(converted_file_name, \
                                     exclusion_list)
-
     else:
         print("File type not supported")
 
     write(data_file, solution_objects)
 
     return (solution_objects)
+
+
+if __name__ == '__main__':
+    readin(sys.argv[0], sys.argv[1])
