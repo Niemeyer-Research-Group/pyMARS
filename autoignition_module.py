9ec6181b6863d91d18d37c96a2cd2526fa804126
parkerclayton <parkerclayton@comcast.net>
2016-06-17 16:52:27 -0700

    working on fixing while loop data output

--- autoignition_module.py
+++ autoignition_module.py
@@ -2,39 +2,68 @@ import sys
 import numpy as np
 import cantera as ct
 import os
+import progressbar as pb
+
 
 def run_sim(solution_objects, sys_args):
     data_file=sys_args.file
+
+
+    """-------------------------------------------------------------------------
+    setup progressbar
+    -------------------------------------------------------------------------"""
+
+    #initialize widgets
+    widgets = ['Time for loop of 1471 iterations: ', pb.Percentage(), ' ',
+                pb.Bar(marker=pb.RotatingMarker()), ' ', pb.ETA()]
+    #initialize timer
+    timer = pb.ProgressBar(widgets=widgets, maxval=166).start() #1471
+
+    """-------------------------------------------------------------------------
+    run sim to find ignition delay from dT/dt max
+    -------------------------------------------------------------------------"""
     #read in solution file, and make constant volume adiabatic reactor
     solution1 = solution_objects[0]
     solution1.TPY = 1001.0, ct.one_atm, 'H2:2,O2:1,N2:4'
     r1 = ct.Reactor(solution1)
     sim1 = ct.ReactorNet([r1])
-    time1 = 0.0
     tnow=0.0
-    tfinal=5.0e-3
-
-
-    """-------------------------------------------------------------------------
-    run sim to find ignition delay from dT/dt max
-    -------------------------------------------------------------------------"""
-
-    times1 = np.zeros(100)
-    data1 = np.zeros((100,2)) #first column is time, second is temperature
-
-    for n in range(100):
-        time1 += 1.e-4
-        sim1.advance(time1)
-        times1[n] = time1 * 1e3  # time in ms
-        data1[n,0] = r1.T
-
+    tfinal=1.0e-3
+    index1 = 0
+    times1 = []
+    temps = [] #first column is time, second is temperature
+    sdata=np.zeros([1, len(r1.Y)])
     while tnow < tfinal:
+
+        index1 += 1
         tnow = sim1.step(tfinal)
-        print(tnow)
+        times1.append(tnow)
+        temps.append(r1.T)
+
+        for i, val in enumerate(r1.Y):
+            species_data=np.array(r1.Y)
+
+            sdata= np.vstack((sdata, species_data))
+
+        timer.update(index1)
+    timer.finish()
+    times1=np.array(times1)
+    temps=np.array(temps)
+    #species_data=species_data[:,np.newaxis].T
+
+    timetemp=np.vstack((times1, temps)).T
+    #timetemporary=timetemp[1,:]
+    #timetemporary=timetemporary[:, np.newaxis].T
+    print(np.shape(sdata))
+    print(np.shape(timetemp))
+    print(index1)
+    #sdata= np.hstack((timetemp, sdata))
+
+
 
 
     #get ignition point from dT/dt
-    T=np.array(data1[:,0])
+    T=np.array(temps)
     dt= np.ones(len(times1)-1)*(times1[1]-times1[0])
     dT= np.diff(T)
     deriv= dT/dt
@@ -49,15 +78,16 @@ def run_sim(solution_objects, sys_args):
 
     #find initial sample point
     for j, dTi in enumerate(dT):
-        if dTi > 15.0:     #when dT > 5 degrees kelvin
+        if dTi > .2:     #when dT > 5 degrees kelvin
             initial_point=[times1[j], T[j], j] # initial point, Temp, index
             break
-        else:
-            #sys.exit("initial sample point not found") #alternative sys exit option
-            initial_point=[times1[i-5], T[i-5], j]
-            print("\nInitial Sample Point not found based on dTmax. Alternative method"+\
-                " of 5 steps before tau is used.")
-            break
+    try:
+        initial_point
+    except NameError:
+        #sys.exit("initial sample point not found") #alternative sys exit option
+        initial_point=[times1[i-20], T[i-20], j]
+        print("\nInitial Sample Point not found based on dTmax. Alternative method"+\
+        " of 5 steps before tau is used.")
 
     #find final sample point
     for k, dti in enumerate(dT):
@@ -65,40 +95,6 @@ def run_sim(solution_objects, sys_args):
             if dti < 1:
                 final_point=[times1[k], T[k], k]    #final point, Temp, index
                 break
-
-
-    """-------------------------------------------------------------------------
-    run sim to get data around ignition
-    -------------------------------------------------------------------------"""
-
-
-    #read in solution file, and make constant volume adiabatic reactor
-    solution2 = solution_objects[0]  #trimmed_h2_v1b_mech.cti
-    solution2.TPY = 1001.0, ct.one_atm, 'H2:2,O2:1,N2:4'
-    r2 = ct.Reactor(solution2)
-    sim2 = ct.ReactorNet([r2])
-
-    time2= initial_point[0]*10e-4
-
-    refined_steps= 200
-    times2= np.zeros(refined_steps)
-    n_species=len(solution2.species_names)
-    data2=np.zeros((refined_steps, n_species+2))
-    species_array=np.ones(n_species)
-    for n in range(refined_steps):
-        time2 +=1.0e-5
-        sim2.advance(time2)
-        times2[n] = time2 * 1e3  # time in ms
-        data2[n, 0] = time2
-        data2[n, 1] = r2.T
-        for i, val in enumerate(r2.Y):                     #get species data
-            species=solution2.species(i).name
-            data2[n, int(i+2)] = val
-        if time2 > final_point[0]*10e-4:        #breaks sim, and trims array
-            data2 = data2[0:n, :]
-            times2=times2[0:n]
-            break
-
 
 
     """-------------------------------------------------------------------------
@@ -111,16 +107,13 @@ def run_sim(solution_objects, sys_args):
         plt.clf()
 
         #plot combustion point
-        plt.plot(deriv_max[0], deriv_max[1], 'ro', label= 'ignition point')
-
+        plt.plot(deriv_max[0], deriv_max[1], 'ro', ms=7, label= 'ignition point')
         #plot initial and final sample points
         plt.plot(initial_point[0], initial_point[1], 'rx', ms=5, mew=2)
         plt.plot(final_point[0], final_point[1], 'rx', ms=5, mew=2)
-
         #plot temp vs time
-        plt.plot(times1, data1[:,0], lw=1.5)
-        plt.plot(times2, data2[:,1], ls='--', lw=2.5, label='sample range')
-        plt.plot(4.0, 1500, 'rx', ms=5, mew=2)
+        plt.plot(times1, temps)
+
         plt.xlabel('Time (ms)')
         plt.title('Mixture Temperature vs Time')
         plt.legend()
@@ -136,10 +129,10 @@ def run_sim(solution_objects, sys_args):
         #format matrix for csv
         names=str(solution2.species_names)
         tt=['Time (ms)', 'Temp (K)']
-        names = solution2.species_names
+        names = solution1.species_names
         name_array = np.append(tt, names)
-        data2=data2.astype('|S10')
-        file_data= np.vstack((name_array, data2))
+        sdata=sdata.astype('|S10')
+        file_data= np.vstack((name_array, sdata))
         #open and write to file
         input_file_name_stripped=os.path.splitext(data_file)[0]
         output_file_name=os.path.abspath('Output_Data_Files/'+ 'species_data_' + input_file_name_stripped + '.csv')
