690c51e0d14aef1801f9c59b9917ae84b0088625
parkerclayton <parkerclayton@comcast.net>
2016-06-20 18:39:01 -0700

    test

--- pyMARS/test_main.py
+++ pyMARS/test_main.py
@@ -5,3 +5,10 @@ class test(object):
         self.var1 = var1
         self.var2 = var2
     variable=2
+
+
+def convert(a, b='none'):
+    value=a
+    other_file=b
+    print value
+    print other_file
