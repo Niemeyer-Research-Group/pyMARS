67e17c432c5f3e776f6dce44c26a0ea28c772cd7
parkerclayton <parkerclayton@comcast.net>
2016-06-23 11:23:12 -0700

    added argv so that module can be called not just from terminal

--- pyMARS/pyMARS.py
+++ pyMARS/pyMARS.py
@@ -8,39 +8,70 @@ from write_to_cti import write
 from autoignition_module import run_sim
 
 
-def readin(args):
+def readin(args='none', **argv):
     """Main function for pyMARS
 
     Arguments
-        --file: Input mechanism file (ex. --file=gri30.cti)
-        --species: Species to eliminate (ex. --species='H, OH')
-        --thermo: Thermo data file if Chemkin format (ex. --thermo= thermo.dat)
-        --transport: Transport data file if Chemkin format
-        --plot
-        --points
-        --writecsv
-        --writehdf5
+        file: Input mechanism file (ex. file='gri30.cti')
+        species: Species to eliminate (ex. species='H, OH')
+        thermo: Thermo data file if Chemkin format (ex. thermo= 'thermo.dat')
+        transport: Transport data file if Chemkin format
+        plot: plot ignition curve (ex. plot='y')
+        points: print ignition point and sample range (ex. points='y')
+        writecsv: write data to csv (ex. writecsv='y')
+        writehdf5: write data to hdf5 (ex. writehdf5='y')
 
     ----------
     Returns
         Converted mechanism file
         Trimmed Solution Object
         Trimmed Mechanism file
+    ----------
+    Example
+        readin(file='gri30.cti', plot='y', species='OH, H')
     """
 
-    data_file= args.file
-    ext= os.path.splitext(data_file)[1]
-    thermo_file = args.thermo
-    transport_file=args.transport
+    if args is 'none':
+        args=argparse.Namespace()
+        args.plot = False
+        args.points = False
+        args.writecsv = False
+        args.writehdf5 = False
 
+        data_file = argv['file']
+        if 'thermo' in argv:
+            thermo_file = argv['thermo']
+        if 'transport' in argv:
+            transport_file = argv['transport']
+        if 'species' in argv:
+            species = argv['species']
+            exclusion_list = [str(item) for item in species.split(',')]
+            print exclusion_list
+        else:
+            exclusion_list=[]
+        if 'plot' in argv:
+            args.plot = True
+        if 'writecsv' in argv:
+            args.writecsv = True
+        if 'writehdf5' in argv:
+            args.writehdf5 = True
+        if 'points' in argv:
+            args.points = True
+        print args
 
-    #if no species are to be trimmed
-    if args.species == None:
-        exclusion_list=[]
     else:
-        exclusion_list=[str(item) for item in args.species.split(',')]
-        print(exclusion_list)
+        data_file= args.file
+        thermo_file = args.thermo
+        transport_file=args.transport
+        if args.species == None:
+            exclusion_list=[]
+        else:
+            exclusion_list=[str(item) for item in args.species.split(',')]
+            print(exclusion_list)
 
+
+    print args
+    ext= os.path.splitext(data_file)[1]
 
     if ext == ".cti" or ext == ".xml":
 
