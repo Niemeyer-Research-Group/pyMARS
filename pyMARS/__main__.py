4d5a77dbb90478445134b39c37bab09d2b035cbe
parkerclayton <parkerclayton@comcast.net>
2016-06-23 19:10:20 -0700

    put sys args in a function

--- pyMARS/__main__.py
+++ pyMARS/__main__.py
@@ -6,53 +6,58 @@ import argparse
 """-------------------------------------------------------------------------
 Get details from command line
 -------------------------------------------------------------------------"""
+def main():
+    #gets arguments from terminal
+    parser=argparse.ArgumentParser(description='pyMARS main: \
+                converts and trims mechanism files \n')
+    parser.add_argument('--file', \
+                        help='input mechanism file name', \
+                        type=str)
 
-#gets arguments from terminal
-parser=argparse.ArgumentParser(description='pyMARS main: \
-            converts and trims mechanism files \n')
-parser.add_argument('--file', \
-                    help='input mechanism file name', \
-                    type=str)
+    parser.add_argument('--species',  \
+                        help='comma separated list input of species to exclude',\
+                        type=str)
 
-parser.add_argument('--species',  \
-                    help='comma separated list input of species to exclude',\
-                    type=str)
+    parser.add_argument('--thermo', \
+                        help='thermodynamic data file', \
+                        type=str)
 
-parser.add_argument('--thermo', \
-                    help='thermodynamic data file', \
-                    type=str)
+    parser.add_argument('--transport', \
+                        help='transport data file', \
+                        type=str)
 
-parser.add_argument('--transport', \
-                    help='transport data file', \
-                    type=str)
+    parser.add_argument('--plot', \
+                        help='plots ignition curve', \
+                        action="store_true")
 
-parser.add_argument('--plot', \
-                    help='plots ignition curve', \
-                    action="store_true")
+    parser.add_argument('--points', \
+                        help='print sim sample points', \
+                        action="store_true")
 
-parser.add_argument('--points', \
-                    help='print sim sample points', \
-                    action="store_true")
+    parser.add_argument('--writecsv', \
+                        help='write species data to csv', \
+                        action="store_true")
 
-parser.add_argument('--writecsv', \
-                    help='write species data to csv', \
-                    action="store_true")
-
-parser.add_argument('--writehdf5', \
-                    help='write species data to hdf5', \
-                    action="store_true")
-
-
+    parser.add_argument('--writehdf5', \
+                        help='write species data to hdf5', \
+                        action="store_true")
 
 
-args=parser.parse_args()
 
-readin(args)
+
+    args=parser.parse_args()
+
+    readin(args)
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 """
 Arguments
-    --file: Input mechanism file (ex. --file=gri30.cti)
+    --file: Input mechanism file (ex. 1--file=gri30.cti)
     --species: Species to eliminate (ex. --species='H, OH')
     --thermo: Thermo data file if Chemkin format (ex. --thermo= thermo.dat)
     --transport: Transport data file if Chemkin format
