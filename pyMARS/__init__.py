e28e43ad6e82accec983544f6217dd44343aeeda
Parker Clayton <parkerclayton@comcast.net>
2016-06-28 11:16:35 -0700

    set local environment variable

--- pyMARS/__init__.py
+++ pyMARS/__init__.py
@@ -3,3 +3,5 @@ from pyMARS import readin
 from convert_chemkin_file import convert
 from write_to_cti import write
 from autoignition_module import run_sim
+import os
+os.environ['Cantera_Data'] =os.getcwd()
