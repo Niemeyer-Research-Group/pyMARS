1ba137ce098fbed585b7ffe5874b703169377d40
Parker Clayton <parkerclayton@comcast.net>
2016-07-13 14:55:35 -0700

    working on drg

--- pyMARS/drg.py
+++ pyMARS/drg.py
@@ -18,36 +18,49 @@ def make_graph(data_file, sim_data):
 
     fname= sim_data
     f= h5py.File(fname, "r")
+    class data(object):
+            times=np.array(f.get('Times'))
+            temps=np.array(f.get('Temps'))
+            species=f.get('Species_Data')
 
-    times=np.array(f.get('Times'))
-    temps=np.array(f.get('Temps'))
-    species_data=f.get('Species_Data')
     
 
+
+    """
+    for n in data.species:
+        if n == 'OH':
+            print n
+            sp =np.array(data.species.get(n))
+            print sp.shape
+
+    sp=sp.astype(float)
+    o=sp[1]
+    z=np.diff(sp)
+    """
+
+    #initalize solution and components
     solution= ct.Solution(data_file)
     species_objects=solution.species()
     reaction_objects=solution.reactions()
+    print len(reaction_objects)
 
     #only want elementary reactions
-
-    for j, rxn in enumerate(reaction_objects):
-        equation_type = type(reaction_objects[j]).__name__
-        if equation_type != 'ElementaryReaction':
-            reaction_objects.remove(reaction_objects[j])
-
-    print (str(len(reaction_objects)) + '  Elementary Reactions\n')
-
-
-
-
-
+    for n, rxn in enumerate(reaction_objects):
+        if rxn.reaction_type is not 1: #1 is for ElementaryReaction
+            reaction_objects.remove(rxn)
 
 
     #Make graph and nodes
-    graph=nx.MultiDiGraph()
+    G=nx.MultiDiGraph()
     for i, species in enumerate(species_objects):
-        graph.add_node(species_objects[i])
+        G.add_node(i, name=species.name)
+    #   G.node[i]['masses'] =
 
+    dt=np.diff(data.times)
+    print type(reaction_objects[0]).__name__
+    for i, t in enumerate(dt):          #enumerate over every time step
+        for n, rx in enumerate(reaction_objects):
+            pass
 
 
 
