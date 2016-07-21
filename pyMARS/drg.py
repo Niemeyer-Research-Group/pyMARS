3b591af9bfc5ff95f11d064c44f26f3cd8788eee
Parker Clayton <parkerclayton@comcast.net>
2016-07-21 10:35:43 -0700

    timestep iteration will need to be deleted

--- pyMARS/drg.py
+++ pyMARS/drg.py
@@ -23,7 +23,6 @@ def make_graph(data_file, sim_data):
             temps=np.array(f.get('Temps'))
             species=f.get('Species_Data')
 
-    
 
 
     """
@@ -42,7 +41,6 @@ def make_graph(data_file, sim_data):
     solution= ct.Solution(data_file)
     species_objects=solution.species()
     reaction_objects=solution.reactions()
-    print len(reaction_objects)
 
     #only want elementary reactions
     for n, rxn in enumerate(reaction_objects):
@@ -50,17 +48,24 @@ def make_graph(data_file, sim_data):
             reaction_objects.remove(rxn)
 
 
-    #Make graph and nodes
+    #Make graph and nodes (each species is a node)
     G=nx.MultiDiGraph()
     for i, species in enumerate(species_objects):
         G.add_node(i, name=species.name)
-    #   G.node[i]['masses'] =
+        mass_values = np.array(data.species.get(species.name))
+        mass_values = mass_values.astype(float) #convert to float
+        G.node[i]['masses'] = mass_values       #assign mass values from simulation to each node
+        G.node[i]['rates'] = np.diff(mass_values)   #get rate values for each timestep
+
+    print len(G.node[0] ['rates'])
 
     dt=np.diff(data.times)
     print type(reaction_objects[0]).__name__
     for i, t in enumerate(dt):          #enumerate over every time step
-        for n, rx in enumerate(reaction_objects):
-            pass
+        if i == 0:                      #temporary measure to only construct DRG for first timestep
+            for n, rx in enumerate(reaction_objects):
+                species_contained= rx.products.keys() + rx.reactants.keys()
+
 
 
 
