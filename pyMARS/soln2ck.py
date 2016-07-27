745b2519701d1a9192cd7bb39aefe115dec9b487
Parker Clayton <parkerclayton@comcast.net>
2016-07-27 16:13:09 -0700

    equation block almost finished. needs TROE formatting

--- pyMARS/soln2ck.py
+++ pyMARS/soln2ck.py
@@ -47,10 +47,7 @@ def write(solution):
     def wrap(input_string):
         output_string= textwrap.fill(input_string, width=60) #subsequent_indent= '                        '
         return output_string
-    def wrap_nasa(input_string):
-        output_string= textwrap.fill(input_string, width=50, \
-                                    subsequent_indent= '                ')
-        return output_string
+
 
     def section_break(title):
         f.write('!'+ "-"*75 + '\n')
@@ -66,39 +63,39 @@ def write(solution):
             coeff_sum=sum(equation_object.reactants.values())
             if equation_type == 'ElementaryReaction':
                 if coeff_sum == 1:
-                    A=str("{:.5E}".format(equation_object.rate.pre_exponential_factor))
+                    A=str("{:.3E}".format(equation_object.rate.pre_exponential_factor))
                 if coeff_sum == 2:
-                    A=str("{:.5E}".format(equation_object.rate.pre_exponential_factor*10**3))
+                    A=str("{:.3E}".format(equation_object.rate.pre_exponential_factor*10**3))
                 if coeff_sum == 3:
-                    A=str("{:.5E}".format(equation_object.rate.pre_exponential_factor*10**6))
+                    A=str("{:.3E}".format(equation_object.rate.pre_exponential_factor*10**6))
                 #if equation_object.duplicate is True:
 
             if equation_type =='ThreeBodyReaction':
                 if coeff_sum == 1:
-                    A=str("{:.5E}".format(equation_object.rate.pre_exponential_factor*10**3))
+                    A=str("{:.3E}".format(equation_object.rate.pre_exponential_factor*10**3))
                 if coeff_sum == 2:
-                    A=str("{:.5E}".format(equation_object.rate.pre_exponential_factor*10**6))
+                    A=str("{:.3E}".format(equation_object.rate.pre_exponential_factor*10**6))
 
             if equation_type !='ElementaryReaction' and equation_type != 'ThreeBodyReaction':
-                A=str("{:.5E}".format(equation_object.rate.pre_exponential_factor)) #*10**6
-            b=equation_object.rate.temperature_exponent
-            E=equation_object.rate.activation_energy/c
+                A=str("{:.3E}".format(equation_object.rate.pre_exponential_factor)) #*10**6
+            b='{:.3f}'.format(equation_object.rate.temperature_exponent)
+            E='{:.2f}'.format(equation_object.rate.activation_energy/c)
             Arr=[ A, b, E]
-            return str(Arr).replace("\'", "")
+            return Arr
 
     def build_mod_Arr(equation_object, t_range):
         if t_range =='high':
-            A=str("{:.5E}".format(equation_object.high_rate.pre_exponential_factor))  #*10**3
-            b=equation_object.high_rate.temperature_exponent
-            E=equation_object.high_rate.activation_energy/c
+            A=str("{:.3E}".format(equation_object.high_rate.pre_exponential_factor))  #*10**3
+            b='{:.3f}'.format(equation_object.high_rate.temperature_exponent)
+            E='{:.2f}'.format(equation_object.high_rate.activation_energy/c)
             Arr_high=[ A, b, E]
-            return str(Arr_high).replace("\'", "")
+            return Arr_high
         if t_range == 'low':
-            A=str("{:.5E}".format(equation_object.low_rate.pre_exponential_factor))   #*10**6
-            b=equation_object.low_rate.temperature_exponent
-            E=equation_object.low_rate.activation_energy/c
+            A=str("{:.3E}".format(equation_object.low_rate.pre_exponential_factor))   #*10**6
+            b='{:.3f}'.format(equation_object.low_rate.temperature_exponent)
+            E='{:.2f}'.format(equation_object.low_rate.activation_energy/c)
             Arr_low=[ A, b, E]
-            return str(Arr_low).replace("\'", "")
+            return Arr_low
 
     def build_falloff(j):
         falloff_str=str(',\n        falloff = Troe(' +
@@ -212,71 +209,30 @@ def write(solution):
         line_4 = line_4_coeffs + '                   4\n'
         f.write(line_4)
 
-
-        """
-        if species.transport.dipole != 0:
-            #string template for each species
-            species_string=Template('species(name = "$name",\n' +
-                            '    atoms = $composition, \n' +
-                            '    thermo = (\n' +
-                            '       NASA(   $nasa_range_1, $nasa_coeffs_1  ),\n' +
-                            '       NASA(   $nasa_range_2, $nasa_coeffs_2  )\n' +
-                            '               ),\n'
-                            '    transport = gas_transport(\n' +
-                            '                   geom = \"$transport_geometry\",\n' +
-                            '                   diam = $diameter, \n' +
-                            '                   well_depth = $well_depth, \n' +
-                            '                   polar = $polar, \n' +
-                            '                   rot_relax = $rot_relax, \n' +
-                            '                   dipole= $dipole) \n' +
-                            '        )\n\n')
-            #write string template
-            f.write(species_string.substitute(name=name, composition=composition, \
-                        nasa_range_1=nasa_range_1, nasa_coeffs_1=nasa_coeffs_1,\
-                        nasa_range_2=nasa_range_2, nasa_coeffs_2=nasa_coeffs_2,\
-                        transport_geometry=transport_geometry, diameter=diameter,\
-                        well_depth=well_depth, polar=polar, rot_relax=rot_relax,\
-                        dipole=dipole))
-        if species.transport.dipole == 0:
-            #string template for each species
-            species_string=Template('species(name = "$name",\n' +
-                            '    atoms = $composition, \n' +
-                            '    thermo = (\n' +
-                            '       NASA(   $nasa_range_1, $nasa_coeffs_1  ),\n' +
-                            '       NASA(   $nasa_range_2, $nasa_coeffs_2  )\n' +
-                            '               ),\n'
-                            '    transport = gas_transport(\n' +
-                            '                   geom = \"$transport_geometry\",\n' +
-                            '                   diam = $diameter, \n' +
-                            '                   well_depth = $well_depth, \n' +
-                            '                   polar = $polar, \n' +
-                            '                   rot_relax = $rot_relax) \n' +
-                            '        )\n\n')
-            #write string template
-            f.write(species_string.substitute(name=name, composition=composition, \
-                        nasa_range_1=nasa_range_1, nasa_coeffs_1=nasa_coeffs_1,\
-                        nasa_range_2=nasa_range_2, nasa_coeffs_2=nasa_coeffs_2,\
-                        transport_geometry=transport_geometry, diameter=diameter,\
-                        well_depth=well_depth, polar=polar, rot_relax=rot_relax,\
-                        ))
-            """
-
+    f.write('END\n')
 
     """-------------------------------------------------------------------------
     Write reactions to file
     -------------------------------------------------------------------------"""
-
     section_break('Reaction Data')
-
+    f.write('REACTIONS\n')
     #write data for each reaction in the Solution Object
     for n, i in enumerate(trimmed_solution.reaction_equations()):
         equation_string=str(trimmed_solution.reaction_equation(n)).upper()
+        equation_string=eliminate(equation_string, ' ', 'single')
         equation_object=trimmed_solution.reaction(n)
         equation_type=type(equation_object).__name__
         m=str(n+1)
 
         #Case if a ThreeBody Reaction
         if equation_type == 'ThreeBodyReaction':
+            arrhenius=build_Arr(equation_object, equation_type)
+            main_line= '{:<41}'.format(equation_string) + \
+                        '{:>9}'.format(arrhenius[0])+\
+                        '{:>9}'.format(arrhenius[1])+\
+                        '{:>11}'.format(arrhenius[2])+\
+                        '\n'
+            f.write(main_line)
 
             #trimms efficiencies list
             efficiencies=equation_object.efficiencies
@@ -285,29 +241,51 @@ def write(solution):
                     if s not in trimmed_solution.species_names:
                         del trimmed_efficiencies[s]
 
-            Arr=build_Arr(equation_object, equation_type)
-            replace_list_2={"{":"\"",      "\'":"",     ": ":":", \
-                                    ",":" ",     "}":"\"" }
+            replace_list_2={'{':'', '}':'/', '\'':'', ':':'/', ',':'/'}
             Efficiencies_string=replace_multiple(str(trimmed_efficiencies).upper(),\
-                        replace_list_2)
+            replace_list_2)
 
-            reaction_string = Template('#  Reaction $m\n' +
-                        'three_body_reaction( \"$equation_string\",  $Arr,\n' +
-                        '       efficiencies = $Efficiencies) \n\n')
+            secondary_line= str(Efficiencies_string) + '\n'
+            f.write(secondary_line)
 
-            f.write(reaction_string.substitute( m=m, \
-                            equation_string=equation_string, Arr=Arr, \
-                            Efficiencies=Efficiencies_string))
         #Case if an elementary Reaction
         if equation_type == 'ElementaryReaction':
-            Arr=build_Arr(equation_object, equation_type)
-            reaction_string=Template('#  Reaction $m\n'+
-                                'reaction( \"$equation_string\", $Arr)\n\n')
-            f.write(reaction_string.substitute(m=m, \
-                            equation_string=equation_string, Arr=Arr))
+            arrhenius=build_Arr(equation_object, equation_type)
+            main_line= '{:<41}'.format(equation_string) + \
+                        '{:>9}'.format(arrhenius[0])+\
+                        '{:>9}'.format(arrhenius[1])+\
+                        '{:>11}'.format(arrhenius[2])+\
+                        '\n'
+            f.write(main_line)
 
         #Case if a FalloffReaction
         if equation_type == 'FalloffReaction':
+            arr_high=build_mod_Arr(equation_object, 'high')
+            main_line= '{:<41}'.format(equation_string) + \
+                        '{:>9}'.format(arr_high[0])+\
+                        '{:>9}'.format(arr_high[1])+\
+                        '{:>11}'.format(arr_high[2])+\
+                        '\n'
+            f.write(main_line)
+
+            arr_low=build_mod_Arr(equation_object, 'low')
+            second_line = '     LOW  /' + \
+                            '  ' + arr_low[0] +\
+                            '  ' + arr_low[1] +\
+                            '  ' + arr_low[2] + '/'+ '\n'
+            f.write(second_line)
+            j=equation_object.falloff.parameters
+            #If optional Arrhenius data included:
+            try:
+                third_line = '     TROE/' + \
+                                '   ' + str(j[0]) +\
+                                '  ' + str(j[1]) +\
+                                '  ' + str(j[2]) +\
+                                '  ' + str(j[3]) +' /' + '\n'
+                f.write(third_line)
+            except (IndexError):
+                pass
+
             #trimms efficiencies list
             efficiencies=equation_object.efficiencies
             trimmed_efficiencies=equation_object.efficiencies
@@ -315,29 +293,13 @@ def write(solution):
                     if s not in trimmed_solution.species_names:
                         del trimmed_efficiencies[s]
 
-            kf=build_mod_Arr(equation_object, 'high')
-            kf0=build_mod_Arr(equation_object, 'low')
-            replace_list_2={"{":"\"",      "\'":"",     ": ":":", \
-                                    ",":" ",     "}":"\"" }
+            replace_list_2={'{':'', '}':'/', '\'':'', ':':'/', ',':'/'}
             Efficiencies_string=replace_multiple(str(trimmed_efficiencies).upper(),\
-                                        replace_list_2)
+            replace_list_2)
 
-            reaction_string = Template('#  Reaction $m\n' +
-                        'falloff_reaction( \"$equation_string\",\n' +
-                        '        kf = $kf,\n' +
-                        '        kf0   = $kf0,\n' +
-                        '        efficiencies = $Efficiencies')
-            f.write(reaction_string.substitute( m=m, \
-                            equation_string=equation_string, kf=kf, kf0=kf0,\
-                            Efficiencies=Efficiencies_string))
-            j=equation_object.falloff.parameters
-            #If optional Arrhenius data included:
-            try:
-                falloff_str=build_falloff(j)
-                f.write(falloff_str)
-            except (IndexError):
-                f.write('\n           )\n\n')
-                pass
+            fourth_line= str(Efficiencies_string) + '\n'
+            f.write(fourth_line)
+    f.write('END')
     return output_file_name
         #print ('trimmed mechanism file:  ' + output_file_name)
     f.close()
