c61365456c04528f712ff2e5ed373fd973c1c736
Parker Clayton <parkerclayton@comcast.net>
2016-08-10 13:27:12 -0700

    updated to take input of mechanism file and hdf5 file, and return hdf5 file of production rates. in future should take solution object

--- pyMARS/get_rate_data.py
+++ pyMARS/get_rate_data.py
@@ -1,28 +1,30 @@
 import cantera as ct
+import h5py
+import numpy as np
 
-def get_rates(mech_file, state_data):
+def get_rates(hdf5_file, mechanism_file):
+    #read in data file
+    f=h5py.File(hdf5_file, 'r')
 
-    times=state_data.time
-    temps=state_data.temp
-    species_data = state_data.sp_data
+    #create file for production rates
+    g=h5py.File('production_rates.hdf5', 'w')
+    #iterate through all 40 timesteps
+    for grp in f:
+        #get solution data at individual timestep
+        group=f[str(grp)]
+        time=group['Time'].value
+        temp=group['Temp'].value
+        mass_fractions=np.array(group['Species Mass Fractions'])
+        #initalize solution, and set state
+        solution=ct.Solution(mechanism_file)
+        solution.Y=mass_fractions
 
-    for i, t in enumerate(times):
-        solution=ct.Solution(mech_file)
-        solution.T = temps[i]
+        #create groups and write production data to new file
+        production_rates=solution.net_production_rates
+        new_grp=g.create_group(str(grp))
+        new_grp['Temp'] = solution.T
+        new_grp['Time'] = time
+        new_grp.create_dataset('Reaction Production Rates', data=production_rates)
+    g.close()
 
-
-
-
-
-
-
-
-
-
-
-
-"for testing"
-from autoignition_module import run_sim
-A=run_sim('gri301.cti')
-
-get_rates('gri301.cti', A)
+get_rates('mass_fractions.hdf5', 'gri30.cti')
