f6cf261dda8f223e1021dbe92aca3f8402f7e827
Parker Clayton <parkerclayton@comcast.net>
2016-08-15 10:46:57 -0700

    takes data from hdf5 mass fraction file produces from augoignition module, and writes a new hdf5 file containing production rates

--- pyMARS/get_rate_data.py
+++ pyMARS/get_rate_data.py
@@ -14,17 +14,24 @@ def get_rates(hdf5_file, mechanism_file):
         group=f[str(grp)]
         time=group['Time'].value
         temp=group['Temp'].value
+        pressure=group['Pressure'].value
         mass_fractions=np.array(group['Species Mass Fractions'])
         #initalize solution, and set state
         solution=ct.Solution(mechanism_file)
-        solution.Y=mass_fractions
+        solution.TPY= temp, pressure, mass_fractions
 
         #create groups and write production data to new file
-        production_rates=solution.net_production_rates
+        species_production_rates=solution.net_production_rates
+        species_production_rates_original=group['Species Net Production Rates Original']
+        reaction_production_rates=solution.net_rates_of_progress
         new_grp=g.create_group(str(grp))
         new_grp['Temp'] = solution.T
         new_grp['Time'] = time
-        new_grp.create_dataset('Reaction Production Rates', data=production_rates)
+        #net production rates for each species (kmol/m^3/s for bulk phases, or kmol/m^2/s for surface)
+        new_grp.create_dataset('Species Net Production Rates', data=species_production_rates)
+        #net rates of progress for each reaction  (kmol/m^3/s for bulk phases, or kmol/m^2/s for surface)
+        new_grp.create_dataset('Reaction Production Rates', data=reaction_production_rates)
+        new_grp.create_dataset('Species Net Production Rates Original', data=species_production_rates_original)
     g.close()
 
-get_rates('mass_fractions.hdf5', 'gri30.cti')
+get_rates('mass_fractions.hdf5', 'gri301.cti')
