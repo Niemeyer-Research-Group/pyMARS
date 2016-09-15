49d4539a1590c5efdfd7884a1cdba07a02b2d34c
parkerclayton <parkerclayton@comcast.net>
2016-09-15 13:32:41 -0700

    changed some variable formatting

--- pyMARS/drg.py
+++ pyMARS/drg.py
@@ -19,32 +19,28 @@ def make_graph(solution_object, hdf5_file, threshold_value, target_species):
     exclusion_list: List of species to trim from mechanism
     """
 
-    f = h5py.File(hdf5_file, 'r')
+    rate_file = h5py.File(hdf5_file, 'r')
 
     #initalize solution and components
     solution = solution_object
     species_objects = solution.species()
     reaction_objects = solution.reactions()
-    #ask for threshold value
-    threshold = threshold_value
-
-    G = nx.MultiGraph()
-    node_labels = {}
+    graph = nx.MultiGraph()
     count = 0
     start_time = time.time()
 
     #add nodes to graph
-    for ind, sp in enumerate(species_objects):
-        G.add_node(sp.name)
+    for species in species_objects:
+        graph.add_node(species.name)
     #iterate through each timestep
-    for nm, grp in f.iteritems():
+    for nm, grp in rate_file.iteritems():
         count += 1
         rxn_prod_rates = np.array(grp['Reaction Production Rates'])
         #generate dict of sum production Rates
         ri_total = {}
         ri_partial = {}
-        for k in species_objects:
-            ri_total[k.name] = 0
+        for species in species_objects:
+            ri_total[species.name] = 0
         #iterate through reactions and build weights
         for i, reac in enumerate(reaction_objects):
             products = reac.products
@@ -93,26 +89,26 @@ def make_graph(solution_object, hdf5_file, threshold_value, target_species):
                 sp_B = both[1]
                 weight = abs(float(ri_partial[ind])/float(ri_total[sp_A]))
                 #only add edge if > than edge value from previous timesteps
-                if weight >= threshold:
-                    if G.has_edge(sp_A, sp_B):
-                        old_weight = G[sp_A][sp_B][0]['weight']
+                if weight >= threshold_value:
+                    if graph.has_edge(sp_A, sp_B):
+                        old_weight = graph[sp_A][sp_B][0]['weight']
                         if weight > old_weight:
-                            G.add_edge(sp_A, sp_B, weight=weight)
+                            graph.add_edge(sp_A, sp_B, weight=weight)
                     else:
-                        G.add_edge(sp_A, sp_B, weight=weight)
+                        graph.add_edge(sp_A, sp_B, weight=weight)
             except IndexError:
                 print ind
                 continue
         progress =  str(count) + '/40 timesteps'
         #print progress
         #print("--- %s seconds ---" % (time.time() - start_time))
-        #print (nm, G.number_of_edges())
+        #print (nm, graph.number_of_edges())
 
     #get connected species
     target = target_species
-    essential_nodes = list(nx.node_connected_component(G, target))
+    essential_nodes = list(nx.node_connected_component(graph, target))
 
-    #nx.draw(G, with_labels=True, width=.25)
+    #nx.draw(graph, with_labels=True, width=.25)
 
     #get list of species to eliminate
     exclusion_list = list()
@@ -127,9 +123,9 @@ def make_graph(solution_object, hdf5_file, threshold_value, target_species):
         exclusion_list_string += spc + ', '
     exclusion_list_string = exclusion_list_string.rstrip(',')
     plt.show(block=False)
-    print G.number_of_edges()
-    print G.number_of_nodes()
-    f.close()
+    print graph.number_of_edges()
+    print graph.number_of_nodes()
+    rate_file.close()
     return ex_list
 
 #make_graph('gri301.cti', 'production_rates.hdf5')
