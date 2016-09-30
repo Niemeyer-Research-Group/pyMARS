975b760a80fa64b3c44f09d44fb5757eee2e8c6a
Parker Clayton <parkerclayton@comcast.net>
2016-09-29 23:32:43 -0700

    fixed error string printing

--- pyMARS/get_sample_range.py
+++ pyMARS/get_sample_range.py
@@ -29,24 +29,29 @@ def get_range(times, temps, sdata, production_data):
         initial_point = [times[index-20], T[index-20], index-20]
     except IndexError:
         initial_point = [times[0], T[0], 0]
-        print 'not enough timesteps before ignition'
-        print 'timesteps before ignition: %s' %i
-        print 'total timesteps: %s' % len(T)
+        error_string = ('not enough timesteps before ignition\n',
+                        'timesteps before ignition: ' + str(index) +'\n',
+                        'total timesteps: ' + str(len(T)))
+    
+        print error_string
     try:
         final_point = [times[index+20], T[index+20], index+20]
     except IndexError:
         final_point = [times[len(times)-1], T[len(T)-1], (len(T)-1)]
-        print 'not enough timesteps after ignition'
-        print 'timesteps after ignition: %s' %(len(T)-i)
-        print 'total timesteps: %s' % len(T)
+        error_string = ('not enough timesteps after ignition\n' +
+                        'timesteps after ignition' + str(len((T))-index) +'\n' +
+                        'total timesteps: ' + str(len(T)))
+        print error_string
+
     class sample_data:
-        def __init__(self, tau, index, times, temps, species_data, production_data, derivative_max, initial_point, final_point):
+        def __init__(self, tau, index, times, temps, species_data,
+                    production_data, derivative_max, initial_point, final_point):
             self.tau = tau
             self.index = index
             self.derivative_max = derivative_max
             self.initial_point = initial_point
             self.final_point = final_point
-            #retain all the data
+
             self.times_total = times
             self.temps_total = temps
             self.species_data_total = species_data
@@ -56,4 +61,5 @@ def get_range(times, temps, sdata, production_data):
             self.temps = temps[index-20:index+20 ]
             self.species_data = species_data[index-20:index+20, :]
             self.production_data = production_data[index-20:index+20, :]
-    return sample_data(tau, index, times, temps, species_data, production_data, derivative_max, initial_point, final_point)
+    return sample_data(tau, index, times, temps, species_data, production_data,
+                        derivative_max, initial_point, final_point)
