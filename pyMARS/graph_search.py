fcd29397e6aec1d6f52b6ec3593d63fe01fa48c1
Parker Clayton <parkerclayton@comcast.net>
2016-09-29 21:55:03 -0700

    added list compare code block

--- pyMARS/graph_search.py
+++ pyMARS/graph_search.py
@@ -14,3 +14,19 @@ def graph_search(solution_object, nx_graph):
     :returns exclusion_list:
         String containing names of species to exclude
     """
+
+    target_species = str(raw_input('Enter target starting species: '))
+    essential_nodes = list(nx.dfs_preorder_nodes(nx_graph, target_species))
+
+    exclusion_list = list()
+    ex_list = []
+
+    for species in solution_object.species():
+        if species.name not in essential_nodes:
+            exclusion_list.append(species.name)
+            ex_list.append(species.name)
+
+    exclusion_list_string = '\''
+    for species in exclusion_list:
+        exclusion_list_string += str(species) + ','
+    exclusion_list_string = exclusion_list_string.rstrip(',')
