e7de24487a62c17618222ca0bf56346148702b9e
Parker Clayton <parkerclayton@comcast.net>
2016-10-03 16:04:21 -0700

    added low temp and high temp parsing

--- pyMARS/autoignition_loop_control.py
+++ pyMARS/autoignition_loop_control.py
@@ -23,11 +23,22 @@ def autoignition_loop_control(solution_object, args='none'):
             initial_sim = False
     if initial_sim is True:
         args.frac = raw_input('Enter mole fractions (ex.CH4:1, O2:2, N2:7.52 for Gri30 Stoich) :  ')
-        args.Temp = float(raw_input('Enter Solution Temperature in (K):'))
+        args.Temp = str(raw_input('Enter Solution Temperature Range in (K) (ex. 800-1000):'))
     else:
         frac = args.frac
         initial_temperature = args.Temp
 
-    sim_result = run_sim(solution_object, args)
+    t_low = float(args.Temp.split('-')[0])
+    t_high = float(args.Temp.split('-')[1])
+    t_array = [t_low]
+    combined_rates = h5py.File('combined_rate_file.hdf5'. 'w')
+    while t_low <= (t_high-100.0):
+        t_low += 100.0
+        t_array.append(t_low)
+    print t_array
+
+    for condition in t_array:
+        args.Temp = float(condition)
+        sim_result = run_sim(solution_object, args)
 
     return sim_result
