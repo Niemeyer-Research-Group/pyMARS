7a1382d7b9355f96fc39e3e76eaf5943cf532b51
Parker Clayton <parkerclayton@comcast.net>
2016-10-05 23:44:58 -0700

    added block to loop autoignition over a given temperature range if told is an initial sim. only does current temp if not

--- pyMARS/autoignition_loop_control.py
+++ pyMARS/autoignition_loop_control.py
@@ -1,5 +1,7 @@
 import cantera as ct
+import h5py
 from autoignition_module import run_sim
+
 
 def autoignition_loop_control(solution_object, args='none'):
     """Controls autoignition module
@@ -16,7 +18,6 @@ def autoignition_loop_control(solution_object, args='none'):
         else:
             initial_sim = False
     else:
-
         if args.initial_sim is True:
             initial_sim = True
         else:
@@ -24,21 +25,25 @@ def autoignition_loop_control(solution_object, args='none'):
     if initial_sim is True:
         args.frac = raw_input('Enter mole fractions (ex.CH4:1, O2:2, N2:7.52 for Gri30 Stoich) :  ')
         args.Temp = str(raw_input('Enter Solution Temperature Range in (K) (ex. 800-1000):'))
+        t_low = float(args.Temp.split('-')[0])
+        t_high = float(args.Temp.split('-')[1])
+        t_array = [t_low]
+        while t_low <= (t_high-100.0):
+            t_low += 100.0
+            t_array.append(t_low)
+        print t_array
+        for condition in t_array:
+            args.Temp = float(condition)
+            print args.Temp
+            sim_result = run_sim(solution_object, args)
     else:
         frac = args.frac
         initial_temperature = args.Temp
-
-    t_low = float(args.Temp.split('-')[0])
-    t_high = float(args.Temp.split('-')[1])
-    t_array = [t_low]
-    combined_rates = h5py.File('combined_rate_file.hdf5', 'w')
-    while t_low <= (t_high-100.0):
-        t_low += 100.0
-        t_array.append(t_low)
-    print t_array
-
-    for condition in t_array:
-        args.Temp = float(condition)
         sim_result = run_sim(solution_object, args)
 
+
+
+    #print sim_result.test
+    #sim_result.test.close()
+    #print sim_result.test
     return sim_result
