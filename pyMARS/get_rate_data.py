3b35b3dd24e1507f631735e86346b09e6daa54ba
Parker Clayton <parkerclayton@comcast.net>
2016-10-12 17:16:47 -0700

    added f.close

--- pyMARS/get_rate_data.py
+++ pyMARS/get_rate_data.py
@@ -57,4 +57,5 @@ def get_rates(hdf5_file, solution_object, initial_temperature):
             new_grp.create_dataset('Reaction Production Rates', data=reaction_production_rates)
             new_grp.create_dataset('Species Net Production Rates Original', data=species_production_rates_original)
     g.close()
+    f.close()
     os.system('rm mass_fractions.hdf5')
