e9e4f79e51fb58e663054291f89a762fcca45572
Parker Clayton <parkerclayton@comcast.net>
2016-10-12 22:19:27 -0700

    added if statement, so that single temperatures can work

--- pyMARS/autoignition_loop_control.py
+++ pyMARS/autoignition_loop_control.py
@@ -14,18 +14,26 @@ def autoignition_loop_control(solution_object, args):
     if args.initial_sim is True:
         args.frac = raw_input('Enter mole fractions (ex.CH4:1, O2:2, N2:7.52 for Gri30 Stoich) :  ')
         args.Temp = str(raw_input('Enter Solution Temperature Range in (K) (ex. 800-1000):'))
-        t_low = float(args.Temp.split('-')[0])
-        t_high = float(args.Temp.split('-')[1])
-        t_array = [t_low]
-        while t_low <= (t_high-100.0):
-            t_low += 100.0
-            t_array.append(t_low)
-        args.temperature_range = t_array
-        for condition in t_array:
-            args.Temp = float(condition)
+        tau_array=[]
+        if '-' in args.Temp:
+            t_low = float(args.Temp.split('-')[0])
+            t_high = float(args.Temp.split('-')[1])
+            t_array = [t_low]
+            while t_low <= (t_high-100.0):
+                t_low += 100.0
+                t_array.append(t_low)
+            for condition in t_array:
+                args.Temp = float(condition)
+
+                tau_array.append(sim_result.tau)
+                #sim_result.test.keys() results in [u'1700.0', u'1800.0']
+            sim_result.tau = tau_array
+            sim_result.initial_temperature_array = t_array
+        else:
+            args.Temp = float(args.Temp)
             sim_result = run_sim(solution_object, args)
-            #sim_result.test.keys() results in [u'1700.0', u'1800.0']
     else:
+
         frac = args.frac
         initial_temperature = args.Temp
         args.Temp = float(raw_input(' else case Enter Solution Temperature to test error in (K) (ex. 800-1000):'))
