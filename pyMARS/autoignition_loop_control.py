0c2f07f487c4eeaac98ec175f65b37a547d1944b
parkerclayton <parkerclayton@comcast.net>
2016-10-17 03:24:16 -0700

    added for loop for conditions, and kept tau and temperature array return

--- pyMARS/autoignition_loop_control.py
+++ pyMARS/autoignition_loop_control.py
@@ -1,6 +1,7 @@
 import cantera as ct
 import h5py
 from autoignition_module import run_sim
+from readin_initial_conditions import readin_conditions
 
 
 def autoignition_loop_control(solution_object, args):
@@ -11,32 +12,21 @@ def autoignition_loop_control(solution_object, args):
     :param args:
         Arguments from terminal
     """
+    conditions_array = readin_conditions(str(args.conditions_file))
+    print 'read in conditions'
     if args.initial_sim is True:
-        args.frac = raw_input('Enter mole fractions (ex.CH4:1, O2:2, N2:7.52 for Gri30 Stoich) :  ')
-        args.Temp = str(raw_input('Enter Solution Temperature Range in (K) (ex. 800-1000):'))
-        args.tau_array=[]
-        if '-' in args.Temp:
-            t_low = float(args.Temp.split('-')[0])
-            t_high = float(args.Temp.split('-')[1])
-            t_array = [t_low]
-            while t_low <= (t_high-100.0):
-                t_low += 200.0
-                t_array.append(t_low)
-            for condition in t_array:
-                args.Temp = float(condition)
-                sim_result = run_sim(solution_object, args)
-                try:
-                    args.tau_array.append(sim_result.tau)
-                except AttributeError:
-                    args.tau_array.append(0.0)
-                #sim_result.test.keys() results in [u'1700.0', u'1800.0']
-            sim_result.tau_array = args.tau_array
-            sim_result.initial_temperature_array = t_array
-        else:
-            args.Temp = float(args.Temp)
-            sim_result = run_sim(solution_object, args)
-            print sim_result.tau_array
+        tau_array = []
+        initial_temperature_array = []
+        for condition in conditions_array:
+            sim_result = run_sim(solution_object, condition, args)
 
+            initial_temperature_array.append(condition.temperature)
+            try:
+                tau_array.append(sim_result.tau)
+            except AttributeError:
+                tau_array.append(0.0)
+        sim_result.tau_array = tau_array
+        sim_result.initial_temperature_array = initial_temperature_array
     else:
         frac = args.frac
         initial_temperature = args.Temp
