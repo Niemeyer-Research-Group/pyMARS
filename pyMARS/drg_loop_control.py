2a956ae9ccb150364aebc881471bbb33651bf941
parkerclayton <parkerclayton@comcast.net>
2016-10-26 23:37:23 -0700

    removed iterate block to start fresh

--- pyMARS/drg_loop_control.py
+++ pyMARS/drg_loop_control.py
@@ -48,33 +48,7 @@ def drg_loop_control(solution_object, args):
         #get_error(new_solution_objects[1], args)
 
     else:
-        #set initial 0 cases
-        error = 0.0
-        threshold = 0.00
-        loop_number = 0
-        error_limit = float(raw_input('Acceptable Error Limit (%): '))
-        while error < error_limit:
-            loop_number += 1
-            threshold +=.01
-            drg_exclusion_list = make_graph(solution_object, 'production_rates.hdf5', threshold, target_species)
-            new_solution_objects = trim(solution_object, drg_exclusion_list, args.data_file)
-            #run second sim with skeletal model to compare ignition delay
-            sim2_result = autoignition_loop_control(new_solution_objects[1], args)
-            tau2 = sim2_result.tau
-            error = float((abs((tau1-tau2)/tau1))*100.0)
-        #re-run auotignition and drg .01 below threshold to get skeletal within acceptable error
-        threshold -= .01
-        drg_exclusion_list = make_graph(solution_object, 'production_rates.hdf5', threshold, target_species)
-        new_solution_objects = trim(solution_object, drg_exclusion_list, args.data_file)
-        #run second sim
-        sim2_result = autoignition_loop_control(new_solution_objects[1], args)
-        #compare error
-        tau2 = sim2_result.tau
-        error = float((abs((tau1-tau2)/tau1))*100.0)
-        print 'Loop number: ' + str(loop_number) + '|| error: ' + str(error) + ' %'
-        print 'Number of loops: %s' %loop_number
-        print 'Final max threshold value: %s' %threshold
-        print 'Error: %s%% ' %"{0:.2f}".format(error)
+        
 
     n_species_eliminated = len(solution_object.species())-len(new_solution_objects[1].species())
     print 'Number of species eliminated: %s' %n_species_eliminated
