da14d36a6fa1665fee857693d92d7eb8e7c8dece
Parker Clayton <parkerclayton@comcast.net>
2016-10-31 10:45:47 -0700

    updated setup file

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ import os
 current_dir=os.getcwd()
 setup(name='pyMARS',
       version='0.1',
+
       description='Python-based (chemical kinetic) Model Automatic \
       Reduction Software (MARS), which consists of multiple techniques for \
       reducing the size and complexity of detailed chemical kinetic models.',
+
       url='http://github.com/kyleniemeyer/pyMARS',
-      author='',
+
+      author='Parker Clayton',
       author_email='claytonp@oregonstate.edu',
       license='MIT',
       packages=find_packages(),
