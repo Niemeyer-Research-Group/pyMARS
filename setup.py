a8910246a73b343b17b16434de7ae6225ffc531a
Parker Clayton <parkerclayton@comcast.net>
2016-10-31 10:43:26 -0700

    updated version number to .1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import os
 current_dir=os.getcwd()
 setup(name='pyMARS',
-      version='0.3',
+      version='0.1',
       description='Python-based (chemical kinetic) Model Automatic \
       Reduction Software (MARS), which consists of multiple techniques for \
       reducing the size and complexity of detailed chemical kinetic models.',
