52bd4e022ea99cd143a7bf4fef5c98226a9ed547
Parker Clayton <parkerclayton@comcast.net>
2016-10-31 11:47:05 -0700

    added condition class for cases where plot is used alone

--- pyMARS/autoignition_loop_control.py
+++ pyMARS/autoignition_loop_control.py
@@ -7,27 +7,49 @@ from readin_initial_conditions import readin_conditions
 def autoignition_loop_control(solution_object, args):
     """Controls autoignition module
 
-    :param solution_object:
+    Parameters
+    ----------
+    solution_object : class
         Cantera solution object
-    :param args:
+    args : class
         Arguments from terminal
+
+    Returns
     """
-    conditions_array = readin_conditions(str(args.conditions_file))
+    try:
+        conditions_array = readin_conditions(str(args.conditions_file))
+    except Exception:
+        class condition():
+            name = str(raw_input('Enter condition #: '))
+            pressure = float(raw_input('Initial Pressure (kPa): '))
+            temperature = float(raw_input('Initial Temperature (K): '))
+            reactants = str(raw_input('List of reactants and mole fractions (ex.Ch4:1,o2:11): '))
+            reactant_list = reactants.split(',')
+            species={}
+            for reactant in reactant_list:
+                species[reactant.split(':')[0]] = reactant.split(':')[1]
+        conditions_array = condition()
+        args.multiple_conditions = False
+
+    tau_array = []
+    initial_temperature_array = []
+
     if args.multiple_conditions is True:
-        tau_array = []
-        initial_temperature_array = []
         for condition in conditions_array:
             sim_result = run_sim(solution_object, condition, args)
-
             initial_temperature_array.append(condition.temperature)
             try:
                 tau_array.append(sim_result.tau)
             except AttributeError:
                 tau_array.append(0.0)
-        sim_result.tau_array = tau_array
-        sim_result.initial_temperature_array = initial_temperature_array
     else:
-        frac = args.frac
-        initial_temperature = args.Temp
-        sim_result = run_sim(solution_object, args)
+        sim_result = run_sim(solution_object, condition, args)
+        initial_temperature_array.append(condition.temperature)
+        try:
+            tau_array.append(sim_result.tau)
+        except AttributeError:
+            tau_array.append(0.0)
+
+    sim_result.tau_array = tau_array
+    sim_result.initial_temperature_array = initial_temperature_array
     return sim_result
