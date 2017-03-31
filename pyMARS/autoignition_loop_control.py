9eb25b6dfd0875532c58ebb8ba3276fd412866a6
Parker Clayton <parkerclayton@comcast.net>
2017-03-30 18:25:47 -0700

    user supplied initial conditions via command line now handled with a class

--- pyMARS/autoignition_loop_control.py
+++ pyMARS/autoignition_loop_control.py
@@ -27,24 +27,35 @@ def autoignition_loop_control(solution_object, args):
             .Temp
             .frac
     """
-    try:
+    class condition:
+        def __init__(self, name):
+            self.name = str(name)
+            self.pressure = float(raw_input('Initial Pressure (kPa): '))
+            self.temperature = float(raw_input('Initial Temperature (K): '))
+            self.reactants = str(raw_input('List of reactants and mole fractions (ex.Ch4:1,o2:11): '))
+            self.reactant_list = self.reactants.split(',')
+            self.species={}
+            for reactant in self.reactant_list:
+                self.species[reactant.split(':')[0]] = reactant.split(':')[1]
+    conditions_array = []
+
+    #get initial conditions from readin, or prompt user if none given
+    if args.conditions_file:
         conditions_array = readin_conditions(str(args.conditions_file))
-    except Exception:
-        class condition():
-            name = str(raw_input('Enter condition #: '))
-            pressure = float(raw_input('Initial Pressure (kPa): '))
-            temperature = float(raw_input('Initial Temperature (K): '))
-            reactants = str(raw_input('List of reactants and mole fractions (ex.Ch4:1,o2:11): '))
-            reactant_list = reactants.split(',')
-            species={}
-            for reactant in reactant_list:
-                species[reactant.split(':')[0]] = reactant.split(':')[1]
-        conditions_array = condition()
-        args.multiple_conditions = False
+    elif not args.conditions_file:
+        print 'No initial conditions file found. Please enter below'
+        number_conditions = int(float(raw_input('Enter # of initial conditions: ')))
+        if number_conditions > 1.0:
+            for i in range(number_conditions):
+                conditions_array.append(condition(i))
+        else:
+            conditions_array.append(condition(1.0))
+            args.multiple_conditions = False
 
     tau_array = []
     initial_temperature_array = []
 
+    #send initial conditions to autoignition script
     if args.multiple_conditions is True:
         for condition in conditions_array:
             sim_result = run_sim(solution_object, condition, args)
@@ -54,8 +65,8 @@ def autoignition_loop_control(solution_object, args):
             except AttributeError:
                 tau_array.append(0.0)
     else:
-        sim_result = run_sim(solution_object, condition, args)
-        initial_temperature_array.append(condition.temperature)
+        sim_result = run_sim(solution_object, conditions_array[0], args)
+        initial_temperature_array.append(conditions_array[0].temperature)
         try:
             tau_array.append(sim_result.tau)
         except AttributeError:
