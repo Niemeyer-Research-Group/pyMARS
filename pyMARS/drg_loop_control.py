7675bc0f3a1e879e95702f3e18192b45278ecbbf
Parker Clayton <parkerclayton@comcast.net>
2017-03-30 20:05:08 -0700

    fixing error reporting

--- pyMARS/drg_loop_control.py
+++ pyMARS/drg_loop_control.py
@@ -34,7 +34,7 @@ def drg_loop_control(solution_object, args):
     detailed_result.test.close()
     ignition_delay_detailed = np.array(detailed_result.tau_array)
     get_rates('mass_fractions.hdf5', solution_object)
-    os.system('rm mass_fractions.hdf5')
+
 
     if args.threshold_values is None:
         try:
@@ -42,10 +42,13 @@ def drg_loop_control(solution_object, args):
         except ValueError:
             print 'try again'
             threshold = float(raw_input('Enter threshold value: '))
+
+        #run DRG and create new reduced solution
         drg = make_graph(solution_object, 'production_rates.hdf5', threshold)
         exclusion_list = graph_search(solution_object, drg, target_species)
         new_solution_objects = trim(solution_object, exclusion_list, args.data_file)
 
+        #simulate reduced solution
         reduced_result = autoignition_loop_control(new_solution_objects[1], args)
         reduced_result.test.close()
         ignition_delay_reduced = np.array(reduced_result.tau_array)
@@ -60,18 +63,26 @@ def drg_loop_control(solution_object, args):
         species_retained = []
         printout = ''
         print 'Threshold     Species in Mech      Error'
+        try:
+            os.system('rm mass_fractions.hdf5')
+        except Exception:
+            pass
         for threshold in threshold_values:
+            try:
+                os.system('rm mass_fractions.hdf5')
+            except Exception:
+                pass
+            #run DRG and create new reduced solution
             drg = make_graph(solution_object, 'production_rates.hdf5', threshold)
             exclusion_list = graph_search(solution_object, drg, target_species)
             new_solution_objects = trim(solution_object, exclusion_list, args.data_file)
             species_retained.append(len(new_solution_objects[1].species()))
 
+            #simulated reduced solution
             reduced_result = autoignition_loop_control(new_solution_objects[1], args)
             reduced_result.test.close()
             ignition_delay_reduced = np.array(reduced_result.tau_array)
             error = (abs(ignition_delay_reduced-ignition_delay_detailed)/ignition_delay_detailed)*100
-            os.system('rm mass_fractions.hdf5')
-            printout += str(threshold) + '  ' + str(len(new_solution_objects[1].species())) + '  '+  str(error) + '\n'
-
+            printout += str(threshold) + '  ' + str(len(new_solution_objects[1].species())) + '  '+  str(np.max(error)) + '\n'
         print printout
     return new_solution_objects
