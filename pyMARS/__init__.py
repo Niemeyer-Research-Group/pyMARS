e0bcfe66f4861dadb098e1d46663aef5c4815431
Phillip MestasIII <mestasp@flip2.engr.oregonstate.edu>
2018-06-07 15:27:57 -0700

    Removal of unneccesary files.  ToDo: Check all comments and documentation.  Final check.  Merge and Pull request.

--- pyMARS/__init__.py
+++
@@ -1,6 +0,0 @@
-from create_trimmed_model import trim
-from pyMARS import readin
-from convert_chemkin_file import convert
-from autoignition_module import run_sim
-import os
-os.environ['Cantera_Data'] =os.getcwd()
