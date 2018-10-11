7693698611e4090a9ff3ede08e3e6f10ef7e2bde
Phillip MestasIII <mestasp@kec1130-16.engr.oregonstate.edu>
2018-10-11 14:56:41 -0700

    Added test for PFA method

--- pymars/tests/test_methods.py
+++ pymars/tests/test_methods.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 import cantera as ct
 
 import drgep
+import pfa
 import sensitivity_analysis
 
 
@@ -46,3 +47,28 @@ def testDRGEPSA():
 	assert len(reduced_model.species()) == len(expected_model.species())
 	assert len(reduced_model.reactions()) == len(expected_model.reactions())
 
+
+def testPFA():
+
+	# Original model
+	path_to_original = os.path.dirname(os.path.abspath(__file__)) + "/../../example_files/gri30.cti" 
+	solution_object = ct.Solution(path_to_original)
+
+	# Conditions for reduction	
+	conditions = os.path.dirname(os.path.abspath(__file__)) + "/../../example_files/example_input_file.txt"
+	error = 5.0
+	target_species = ["CH4","O2"]
+	retained_species = ["CH4","O2","N2","H2O","CO2"]
+	final_error = [0]
+	epsilon_star = .5
+
+	# Run PFA
+	reduced_model = pfa.run_pfa(solution_object, conditions, error, target_species, retained_species, path_to_original, final_error)
+
+	# Expected answer	
+	path_to_answer = os.path.dirname(os.path.abspath(__file__)) + "/pfa_gri30.cti"
+	expected_model = ct.Solution(path_to_answer)
+
+	# Make sure models are the same
+	assert len(reduced_model.species()) == len(expected_model.species())
+	assert len(reduced_model.reactions()) == len(expected_model.reactions())
