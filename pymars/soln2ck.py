41af00351e20b2a9062846cab26f4a86a004368f
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2019-05-16 14:10:23 -0700

    WiP for soln2ck refactor [skip ci]

--- pymars/soln2ck.py
+++ pymars/soln2ck.py
@@ -5,150 +5,143 @@ Cantera development version 2.3.0a2 required
 """
 
 import os
-from string import Template
 
 import cantera as ct
-from cantera import ck2cti
 
 # number of calories in 1000 Joules
 CALORIES_CONSTANT = 4184.0
 
 
-def eliminate(input_string, char_to_replace, spaces='single'):
-    """Eliminate characters from a string
+def section_break(section_title):
+    """Return string with break and new section title
 
     Parameters
     ----------
-    input_string : str
-        string to be modified
-    char_to_replace : list of str
-        array of character strings to be removed
-    
-    Returns
-    -------
-    input_string : str
-        string with characters eliminated
-    
-    """
-    for char in char_to_replace:
-        input_string = input_string.replace(char, "")
-    if spaces == 'double':
-        input_string = input_string.replace(" ", "  ")
-    return input_string
-
-def section_break(file, section_title):
-    """Insert break and new section title into file
-
-    Parameters
-    ----------
-    file : io.TextIOWrapper
-        Open file handle for writing
     section_title : str
         title string for next section_break
-
-    """
-    file.write('!'+ "-"*75 + '\n')
-    file.write('!  ' + section_title +'\n')
-    file.write('!'+ "-"*75 + '\n')
-
-def replace_multiple(input_string, replace_list):
-    """Replace multiple characters in a string
-
-    Parameters
-    ----------
-    input_string : str
-        string to be modified
-    replace_list : dict
-        dict containing items to be replaced (value replaces key)
     
     Returns
     -------
-    input_string : str 
-        string with characters replaced
+    str
+        String with section title and breaks
 
     """
-    for original_character, new_character in replace_list.items():
-        input_string = input_string.replace(original_character,
-                                            new_character)
-    return input_string
+    return('!'+ '-' * 75 + '\n' +
+           f'!  {section_title}\n' +
+           '!'+ '-' * 75 + '\n'
+           )
 
 
-def build_arrhenius(reaction):
-    """Builds Arrhenius coefficient string
+def build_arrhenius(rate, reaction_order, reaction_type):
+    """Builds Arrhenius coefficient string based on reaction type.
 
     Parameters
     ----------
-    reaction : cantera.Reaction
-        cantera reaction object
-    
+    rate : cantera.Arrhenius
+        Arrhenius-form reaction rate coefficient
+    reaction_order : int or float
+        Order of reaction (sum of reactant stoichiometric coefficients)
+    reaction_type : {cantera.ElementaryReaction, cantera.ThreeBodyReaction, cantera.PlogReaction}
+        Type of reaction
+
     Returns
     -------
-    list of str
-        list of strings with Arrhenius coefficients
+    str
+        String with Arrhenius coefficients
 
     """
-    coeff_sum = sum(reaction.reactants.values())
-    pre_exponential_factor = reaction.rate.pre_exponential_factor
-    temperature_exponent = '{:.3f}'.format(reaction.rate.temperature_exponent)
-    activation_energy = '{:.2f}'.format(reaction.rate.activation_energy / CALORIES_CONSTANT)
+    if reaction_type in [ct.ElementaryReaction, ct.PlogReaction]:
+        pre_exponential_factor = rate.pre_exponential_factor * 1e3**(reaction_order - 1)
 
-    if type(reaction) == ct.ElementaryReaction:
-        if coeff_sum == 1:
-            pre_exponential_factor = str('{:.3E}'.format(pre_exponential_factor))
-        elif coeff_sum == 2:
-            pre_exponential_factor = str('{:.3E}'.format(pre_exponential_factor * 1e3))
-        elif coeff_sum == 3:
-            pre_exponential_factor = str('{:.3E}'.format(pre_exponential_factor * 1e6))
-    elif type(reaction) == ct.ThreeBodyReaction:
-        if coeff_sum == 1:
-            pre_exponential_factor = str('{:.3E}'.format(pre_exponential_factor * 1e3))
-        elif coeff_sum == 2:
-            pre_exponential_factor = str('{:.3E}'.format(pre_exponential_factor * 1e6))
-    elif type(reaction) not in [ct.ElementaryReaction, ct.ThreeBodyReaction]:
-        pre_exponential_factor = str('{:.3E}'.format(pre_exponential_factor))
+    elif reaction_type == ct.ThreeBodyReaction:
+        pre_exponential_factor = rate.pre_exponential_factor * 1e3**reaction_order
+
+    elif reaction_type in [ct.FalloffReaction, ct.ChemicallyActivatedReaction]:
+        raise ValueError('Function does not support falloff or chemically activated reactions')
+    else:
+        raise NotImplementedError('Reaction type not supported: ', reaction_type)
     
-    return [pre_exponential_factor, temperature_exponent, activation_energy]
+    arrhenius = [f'{pre_exponential_factor:.4e}', 
+                 f'{rate.temperature_exponent:.3f}', 
+                 f'{(rate.activation_energy / CALORIES_CONSTANT):.:.3e}'
+                 ]
+    return '  '.join(arrhenius)
 
 
-def build_modified_arrhenius(reaction, pres_range):
-    """Builds Arrhenius coefficient strings for high and low pressure ranges
+def build_falloff_arrhenius(rate, reaction_order, reaction_type, pressure_limit):
+    """Builds Arrhenius coefficient strings for falloff and chemically-activated reactions.
 
     Parameters
     ----------
-    reaction : cantera.Reaction
-        cantera reaction object
-    pres_range : str
-        simple string ('high' or 'low') to designate pressure range
+    rate : cantera.Arrhenius
+        Arrhenius-form reaction rate coefficient
+    reaction_order : int or float
+        Order of reaction (sum of reactant stoichiometric coefficients)
+    reaction_type : {ct.FalloffReaction, ct.ChemicallyActivatedReaction}
+        Type of reaction
+    pressure_limit : {'high', 'low'}
+        string designating pressure limit
     
     Returns
     -------
     str
         Arrhenius coefficient string
-        
+
     """
-    if pres_range == 'high':
-        pre_exponential_factor = reaction.high_rate.pre_exponential_factor
-        temperature_exponent = '{:.3f}'.format(reaction.high_rate.temperature_exponent)
-        activation_energy = '{:.2f}'.format(reaction.high_rate.activation_energy/CALORIES_CONSTANT)
-        if len(reaction.products) == 1:
-            pre_exponential_factor = str('{:.5E}'.format(pre_exponential_factor * 1e3))
-        else:
-            pre_exponential_factor = str('{:.5E}'.format(pre_exponential_factor))
+    assert pressure_limit in ['low', 'high'], 'Pressure range needs to be high or low'
 
-        return [pre_exponential_factor, temperature_exponent, activation_energy]
-    elif pres_range == 'low':
+    # Each needs more complicated handling due if high- or low-pressure limit
+    if reaction_type == ct.FalloffReaction:
+        if pressure_limit == 'low':
+            pre_exponential_factor = rate.pre_exponential_factor * 1e3**(reaction_order)
+        elif pressure_limit == 'high':
+            pre_exponential_factor = rate.pre_exponential_factor * 1e3**(reaction_order - 1)
 
-        pre_exponential_factor = reaction.low_rate.pre_exponential_factor
-        temperature_exponent = '{:.3f}'.format(reaction.low_rate.temperature_exponent)
-        activation_energy = '{:.2f}'.format(reaction.low_rate.activation_energy/CALORIES_CONSTANT)
-        if len(reaction.products) == 1:
-            pre_exponential_factor = str('{:.5E}'.format(pre_exponential_factor * 1e6))
-        else:
-            pre_exponential_factor = str('{:.5E}'.format(pre_exponential_factor * 1e3))
-
-        return [pre_exponential_factor, temperature_exponent, activation_energy]
+    elif reaction_type == ct.ChemicallyActivatedReaction:
+        if pressure_limit == 'low':
+            pre_exponential_factor = rate.pre_exponential_factor * 1e3**(reaction_order - 1)
+        elif pressure_limit == 'high':
+            pre_exponential_factor = rate.pre_exponential_factor * 1e3**(reaction_order - 2)
     else:
-        raise ValueError('Pressure range needs to be high or low')
+        raise ValueError('Reaction type not supported: ', reaction_type)
+
+    arrhenius = [f'{pre_exponential_factor:.4e}', 
+                 f'{rate.temperature_exponent:.3f}', 
+                 f'{(rate.activation_energy / CALORIES_CONSTANT):.:.3e}'
+                 ]
+    return '  '.join(arrhenius)
+
+
+def build_falloff(parameters, falloff_function):
+    """Creates falloff reaction Troe parameter string
+
+    Parameters
+    ----------
+    parameters : numpy.ndarray
+        Array of falloff parameters; length varies based on ``falloff_function``
+    falloff_function : {'Troe', 'SRI'}
+        Type of falloff function
+
+    Returns
+    -------
+    falloff_string : str
+        String of falloff parameters
+
+    """
+    if falloff_function == ct.TroeFalloff:
+        falloff_string = ('TROE / ' +
+                          f'{parameters[0]}  {parameters[1]}  '
+                          f'{parameters[2]}  {parameters[3]} /\n'
+                          )
+    elif falloff_function == ct.SriFalloff:
+        falloff_string = ('SRI / ' + 
+                          f'{parameters[0]}  {parameters[1]}  ' +
+                          f'{parameters[2]}  {parameters[3]}  {parameters[4]} /\n'
+                          )
+    else:
+        raise NotImplementedError(f'Falloff function not supported: {falloff_function}')
+
+    return falloff_string
 
 
 def build_nasa(nasa_coeffs, row):
@@ -176,36 +169,6 @@ def build_nasa(nasa_coeffs, row):
                 line_coeffs += ' '
             line_coeffs += str('{:.8e}'.format(c))
     return line_coeffs
-
-
-def build_species_string(trimmed_solution):
-    """Formats species list at top of mechanism file
-    
-    Parameters
-    ----------
-    trimmed_solution : cantera.Solution
-        Solution object for reduced model
-
-    Returns
-    -------
-    species_list_string : str
-        String with species list
-
-    """
-    species_list_string = ''
-    line = 1
-    for sp_string in trimmed_solution.species_names:
-        sp = ' '
-        #get length of string next species is added
-        length_new = len(sp_string)
-        length_string = len(species_list_string)
-        total = length_new + length_string + 3
-        #if string will go over width, wrap to new line
-        if total >= 70*line:
-            species_list_string += '\n'
-            line += 1
-        species_list_string += sp_string + ((16-len(sp_string))*sp)
-    return species_list_string
 
 
 def write(solution):
@@ -385,33 +348,4 @@ def write(solution):
 
         the_file.write('END')
 
-    #Test mechanism file
-
-    original_solution = solution
-    #convert written chemkin file to cti, and get solution
-    parser = ck2cti.Parser()
-    outName = 'test_file.cti'
-    parser.convertMech(output_file_name, outName=outName)
-    new_solution = ct.Solution(outName)
-
-    #TODO: test new solution vs original solution
-    #test(original_solution, new_solution)
-    os.remove(outName)
     return output_file_name
-
-
-    """
-    def build_falloff(j):
-
-        Creates falloff reaction Troe parameter string
-
-        param j:
-            Cantera falloff parameters object
-
-        falloff_str = str(',\n        falloff = Troe(' +
-                        'A = ' + str(j[0]) +
-                        ', T3 = ' + str(j[1]) +
-                        ', T1 = ' + str(j[2]) +
-                        ', T2 = ' + str(j[3]) +')       )\n\n')
-        return falloff_str
-    """
